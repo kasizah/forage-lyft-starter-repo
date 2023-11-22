ff36224b02469d58324c0ef6160fc88735df3d1d
Karis Caruso <cchristophercmc@gmail.com>
2023-11-21 22:27:34 -0500

    value swap

--- test/test_battery.py
+++ test/test_battery.py
@@ -7,25 +7,25 @@ from battery.spindler_battery import SpindlerBattery
 class TestSpindler(unittest.TestCase):
     def test_battery_should_be_serviced(self):
         current_date = datetime.today().date()
-        last_service_date = current_date.replace(year=current_date.year - 5)
+        last_service_date = current_date.replace(year=current_date.year - 3)
         
         self.assertTrue(SpindlerBattery(current_date, last_service_date).needs_service())
 
     def test_battery_should_not_be_serviced(self):
         current_date = datetime.today().date()
-        last_service_date = current_date.replace(year=current_date.year - 3)
+        last_service_date = current_date.replace(year=current_date.year - 1)
 
         self.assertFalse(SpindlerBattery(current_date, last_service_date).needs_service())
 
 class TestNubbin(unittest.TestCase):
     def test_battery_should_be_serviced(self):
         current_date = datetime.today().date()
-        last_service_date = current_date.replace(year=current_date.year - 3)
+        last_service_date = current_date.replace(year=current_date.year - 5)
 
         self.assertTrue(NubbinBattery(current_date, last_service_date).needs_service())
 
     def test_battery_should_not_be_serviced(self):
         current_date = datetime.today().date()
-        last_service_date = current_date.replace(year=current_date.year - 1)
+        last_service_date = current_date.replace(year=current_date.year - 3)
 
         self.assertFalse(NubbinBattery(current_date, last_service_date).needs_service())
